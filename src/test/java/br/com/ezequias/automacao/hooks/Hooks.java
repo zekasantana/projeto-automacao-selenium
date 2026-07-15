7f795e26224c1b83f13c08f9516e371a9d8fa78c
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-15 12:43:28 -0300

    feat: adciona captura automatica de evidências em falha

--- src/test/java/br/com/ezequias/automacao/hooks/Hooks.java
+++ src/test/java/br/com/ezequias/automacao/hooks/Hooks.java
@@ -3,6 +3,13 @@ package br.com.ezequias.automacao.hooks;
 import br.com.ezequias.automacao.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hooks {
 
@@ -12,7 +19,29 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
+    public void after(Scenario scenario) {
+        if (scenario.isFailed()) {
+
+            File screenshot =
+                    ((TakesScreenshot) DriverFactory.getDriver())
+                            .getScreenshotAs(OutputType.FILE);
+            try {
+
+                FileUtils.copyFile(
+                        screenshot,
+                        new File(
+                                "evidencias/"
+                                        + scenario.getName()
+                                        .replace(" ", "_")
+                                        + ".png"
+                        )
+                );
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         DriverFactory.quitDriver();
     }
 }
