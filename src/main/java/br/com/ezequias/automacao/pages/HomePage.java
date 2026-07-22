3bcdca62e62fa630924cfac6b79a672f65d6d466
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-22 16:45:58 -0300

    feat: add PMD static code analysis

--- src/main/java/br/com/ezequias/automacao/pages/HomePage.java
+++ src/main/java/br/com/ezequias/automacao/pages/HomePage.java
@@ -1,14 +1,9 @@
 package br.com.ezequias.automacao.pages;
 
-import org.openqa.selenium.By;
-
 public class HomePage extends BasePage {
-
-    private final By loginMenu = By.linkText("Log in");
 
     public void acessarHome() {
         driver.get("https://demowebshop.tricentis.com/");
     }
-
 
 }
